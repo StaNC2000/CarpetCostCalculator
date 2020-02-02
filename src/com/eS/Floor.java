a5bb63d36749ecd45965df8a6b115e4bb158d0e6
es <StaNC2000>
2020-02-02 14:00:27 +0100

    Floor class created with method

--- src/com/eS/Floor.java
+++ src/com/eS/Floor.java
@@ -1,4 +1,25 @@
 package com.eS;
 
 public class Floor {
+
+    private double width;
+    private double length;
+
+    public Floor(double width, double length){
+        if (width < 0) {
+            this.width = 0;
+        }
+        else if (length < 0) {
+            this.length = 0;
+        }
+        else {
+            this.width = width;
+            this.length = length;
+        }
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
 }
