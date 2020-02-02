0f7b98c386ff563eb475ebf2abfa0ba23c78d8ce
es <StaNC2000>
2020-02-02 14:13:55 +0100

    Calculator class created with method

--- src/com/eS/Calculator.java
+++ src/com/eS/Calculator.java
@@ -1,4 +1,21 @@
 package com.eS;
 
 public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+
+        double totalCost = floor.getArea() * carpet.getCost();
+
+        return totalCost;
+
+    }
+
 }
